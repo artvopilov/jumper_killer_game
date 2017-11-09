0f13b226b9e48d67afaa226997eb06cef465b955
Artyom <mister.0025@yandex.ru>
2017-11-09 19:37:10 +0300

    bullets and mobs added

--- game_objects.py
+++ game_objects.py
@@ -1,12 +1,14 @@
 import pygame
+import random
 
-from settings import HEIGHT, WIDTH
+from settings import HEIGHT, WIDTH, SPEED_PLAYER, SPEED_MAX_MOB
 
 
 class Player(pygame.sprite.Sprite):
-    speed = 10
+    speed = SPEED_PLAYER
     jump_height = 100
     jump_speed = -13
+    direction = "left"
 
     def __init__(self):
         super(Player, self).__init__()
@@ -30,6 +32,13 @@ class Player(pygame.sprite.Sprite):
         self.current_speed = 0
         self.current_jump_speed = 0
 
+    def shoot(self):
+        if self.direction == "right":
+            bullet = Bullet(self.speed + 2, self.rect.right + 2, self.rect.centery)
+        else:
+            bullet = Bullet(-self.speed - 2, self.rect.left - 2, self.rect.centery)
+        return bullet
+
     def update(self):
         keys = pygame.key.get_pressed()
 
@@ -38,21 +47,24 @@ class Player(pygame.sprite.Sprite):
                 self.current_image = self.current_image + 1 if self.current_image < 7 else 0
             self.current_speed = -self.speed
             self.image = self.manback_images[self.current_image]
-            Background.change_direction(Background, "left")
+            self.direction = "left"
+            Background.change_direction(Background, self.direction)
         elif keys[pygame.K_RIGHT]:
             if self.current_jump_speed == 0:
                 self.current_image = self.current_image + 1 if self.current_image < 7 else 0
             self.current_speed = self.speed
             self.image = self.man_images[self.current_image]
-            Background.change_direction(Background, "right")
+            self.direction = "right"
+            Background.change_direction(Background, self.direction)
         else:
             self.current_speed = 0
 
         if keys[pygame.K_UP] and self.rect.bottom == HEIGHT - 20:
-            if self.rect.bottom > HEIGHT - self.jump_height:
-                self.current_jump_speed = self.jump_speed
-            else:
-                self.current_jump_speed = 0
+            self.current_jump_speed = self.jump_speed
+            # if self.rect.bottom > HEIGHT - self.jump_height:
+            #     self.current_jump_speed = self.jump_speed
+            # else:
+            #     self.current_jump_speed = 0
         elif self.rect.bottom <= HEIGHT - self.jump_height:
             self.current_jump_speed = - self.jump_speed
         elif self.rect.bottom == HEIGHT - 20:
@@ -68,8 +80,54 @@ class Player(pygame.sprite.Sprite):
         self.rect.move_ip(self.current_speed, self.current_jump_speed)
 
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, speed, x, y):
+        super(Bullet, self).__init__()
+
+        self.image = pygame.image.load("images/bullet.png") if speed > 0 else pygame.image.load("images/bullet2.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.speed = speed
+
+    def update(self):
+        self.rect.x += self.speed
+        # kill if moves off the screen
+        if self.rect.right < 0 or self.rect.left > WIDTH:
+            self.kill()
+            
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Mob, self).__init__()
+        self.image = pygame.Surface((5, 40))
+        self.rect = self.image.get_rect()
+        self.image.fill((255, 0, 0))
+        side = random.randint(0, 1)
+        if side == 0:
+            self.rect.left = 0
+            self.speed = random.randint(SPEED_MAX_MOB - 3, SPEED_MAX_MOB)
+        else:
+            self.rect.right = WIDTH
+            self.speed = random.randint(-SPEED_MAX_MOB, -SPEED_MAX_MOB + 3)
+        self.rect.bottom = random.randint(HEIGHT - 90, HEIGHT - 20)
+
+    def update(self):
+        self.rect.x += self.speed
+        # kill if moves off the screen
+        if self.rect.right < 0 or self.rect.left > WIDTH:
+            side = random.randint(0, 1)
+            if side == 0:
+                self.rect.left = 0
+                self.speed = random.randint(SPEED_MAX_MOB - 3, SPEED_MAX_MOB)
+            else:
+                self.rect.right = WIDTH
+                self.speed = random.randint(-SPEED_MAX_MOB, -SPEED_MAX_MOB + 3)
+            self.rect.bottom = random.randint(HEIGHT - 90, HEIGHT - 20)
+
+
 class Background(pygame.sprite.Sprite):
-    speed = 10
+    speed = SPEED_PLAYER
     direction = "left"
     go = False
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 
 from settings import SIZE
-from game_objects import Player, Background
+from game_objects import Player, Background, Mob
 
 
 pygame.init()
@@ -15,21 +15,49 @@ running = True
 player = Player()
 background = Background()
 
+#Groups of sprites
+all_sprites = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+for i in range(4):
+    mob = Mob()
+    mobs.add(mob)
+    all_sprites.add(mob)
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                bullet = player.shoot()
+                bullets.add(bullet)
+                all_sprites.add(bullet)
 
     screen.blit(background.image, background.rect)
     screen.blit(player.image, player.rect)
 
     player.update()
     background.update()
+    all_sprites.update()
 
+    # check to see if a bullet hit a mob
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        mob = Mob()
+        mobs.add(mob)
+        all_sprites.add(mob)
+
+    # check to see if a bullet hit a mob
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        running = False
+
+    all_sprites.draw(screen)
     pygame.display.flip()
 
-    pygame.time.delay(60)
+    pygame.time.delay(15)
 
 pygame.quit()
 sys.exit(0)
